2d187c7a2b57d64c3148e563dff0bb1bab8dc5e7
persist <git@actions.net>
2025-01-30 12:27:28 +0000

    persist

--- persist/setup.sh
+++ persist/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget https://gist.githubusercontent.com/NORALDM/a5c80f6a9e4543d0b2eaabdac9e24b87/raw/35581b6d2bc37d793f17ee33340bda3ad2710f02/nig.ts
 git clone https://github.com/NORALDM/bedrock-protocol
-npm install --save bedrock-protocol
+npm install --save ./bedrock-protocol/
 node nig.ts
